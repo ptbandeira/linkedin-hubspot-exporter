ec41c4b276767503568f2a8e60cec146cf5fc18a
Flagster <63302966+ptbandeira@users.noreply.github.com>
2024-11-21 14:32:53 +0100

    Create background.js

---
+++ background.js
@@ -0,0 +1,5 @@
+chrome.runtime.onInstalled.addListener(() => {
+    console.log('LinkedIn to HubSpot Contact Exporter installed');
+});
+
+// Optional: Add any background processing logic here if needed
